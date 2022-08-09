077193863531b1becf34250bc8bb2e8e7f029200
JACaddock <jamesacaddock@gmail.com>
2022-08-09 13:40:35 +0100

    Updated README and metadata

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Metronome from './components/Metronome';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Metronome />
+      <header>
+        <h1>Metronome Web App</h1>
       </header>
+      <div>
+        <Metronome  className="flex" />
+      </div>
     </div>
   );
 }
